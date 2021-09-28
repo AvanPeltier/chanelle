03b27540d23968c64c8e1b63c2cba3d8c8538d8c
Avan Peltier <tex@csh.rit.edu>
2021-09-28 15:50:50 -0400

    Attempting better error checking

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import random
 
 from flask import Flask
 from slack import WebClient
+from slack_sdk.errors import SlackApiError
 from slackeventsapi import SlackEventAdapter
 
 app = Flask(__name__)
@@ -28,24 +29,29 @@ def hello_world():
 
 @slack_events_adapter.on("message")
 def message(payload):
-    event = payload.get("event", {})
-    text = event.get("text")
-    if "flip a coin" in text.lower():
-        channel_id = event.get("channel")
+    try:
+        event = payload.get("event", {})
+        text = event.get("text")
+        if "flip a coin" in text.lower():
+            channel_id = event.get("channel")
 
-        rand_int = random.randint(0, 1)
+            rand_int = random.randint(0, 1)
 
-        if rand_int == 0:
-            results = "Heads"
-        else:
-            results = "Tails"
+            if rand_int == 0:
+                results = "Heads"
+            else:
+                results = "Tails"
 
-        output = f"The result is {results}"
+            output = f"The result is {results}"
 
-        MESSAGE_BLOCK["text"]["text"] = output
-        message_to_send = {"channel": channel_id, "blocks": MESSAGE_BLOCK}
+            MESSAGE_BLOCK["text"]["text"] = output
+            message_to_send = {"channel": channel_id, "blocks": MESSAGE_BLOCK}
 
-        return slack_web_client.chat_postMessage(**message_to_send)
+            return slack_web_client.chat_postMessage(**message_to_send)
+    except SlackApiError as e:
+        assert e.response["Success"] is False
+        assert e.response["ERROR"]
+        print(f"Got an error: {e.response['ERROR']}")
 
 
 if __name__ == '__main__':
